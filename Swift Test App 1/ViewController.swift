04ea22187028754fb86b47590f1714130b40dbb2
Geraldo Mercado <gerrymercado@geraldos-mac.home>
2017-02-01 14:16:48 -0500

    Initial Commit

---
+++ Swift Test App 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Test App 1
+//
+//  Created by Geraldo Mercado on 2/1/17.
+//  Copyright © 2017 GerryMercado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
