b00a42e68c0511fc73bd5fc4628b69c9b0a4bbac
GMercado72 <gmercado72@gmail.com>
2017-02-02 15:55:10 -0500

    1st commit

--- Swift Test App 1/ViewController.swift
+++ Swift Test App 1/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var thelabel: UILabel!
+    
+   var tapCount1 = 0
+    
+    
+        @IBAction func buttonTap(_ sender: Any) {
+        thelabel.text = "Am I Here"
+        self.view.backgroundColor = UIColor.white
+        print("button tapped")
+            
+          
+            tapCount1 = tapCount1 + 1
+
+            if tapCount1 >= 5 { thelabel.text = "YESSSSS?"}
+    
+
+            
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    
 
+}
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
